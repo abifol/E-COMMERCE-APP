3cb152b02ce18b952bd7bcda17c67c6c33c8f951
SuDaniels <oluwaseundaniels17@gmail.com>
2024-03-04 23:43:54 +0100

    New change

---
+++ src/pages/Services.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Nav from "../Components/navbar/Nav";
+import Navbar from "../Components/navbar/Navbar";
+import Hero from "../Components/hero/hero";
+import Hero2 from "../Components/hero/hero2";
+import Hero3 from "../Components/hero/hero3";
+
+const Services = () => {
+  return (
+    <div>
+      <Nav />
+      <Navbar />
+      <Hero />
+      <Hero2 />
+      <Hero3 />
+    </div>
+  );
+};
+
+export default Services;
