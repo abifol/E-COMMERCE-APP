a069aaf106f096ce9d9d15cb7d6522435189f88d
abifol <abifol479@gmail.com>
2024-03-04 18:18:58 +0100

    updating branch

--- src/components/hero.jsx
+++ src/components/hero.jsx
@@ -1,20 +1,39 @@
 import React from "react";
-import Banner from "../assets/banner.png";
 import "../style/hero.css";
+import banner1 from "../assets/Link → banner-41.jpg.png";
+import banner2 from "../assets/Link → banner-42.jpg.png";
 
 const hero = () => {
   return (
     <div>
-      <div class="card text-center">
-        <div class="card-body">
-          <h5 class="card-title">Special title treatment</h5>
-          <p class="card-text">
-            With supporting text below as a natural lead-in to additional
-            content.
-          </p>
-          <a href="#" class="btn btn-primary">
-            Go somewhere
-          </a>
+      <div class="row">
+        <div class="col-sm-6">
+          <div class="card">
+            <img src={banner1} />
+            <div class="card-body">
+              <h5 class="card-title">Bring your ideas to life in minute</h5>
+              <p class="card-text">
+                Print shirts for yourself or your creative works
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
+        </div>
+        <div class="col-sm-6">
+          <div class="card">
+            <img src={banner2} />
+            <div class="card-body">
+              <h5 class="card-title">T-shirt printing made easy.</h5>
+              <p class="card-text">
+                Create your design for your online business
+              </p>
+              <a href="#" class="btn btn-primary">
+                Go somewhere
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- src/pages/home.jsx
+++ src/pages/home.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import Hero from "../components/hero";
+import Banner from "../components/banner";
+import Gallery from "../components/gallery";
 
 const home = () => {
   return (
     <div>
       <Navbar />
       <Hero />
+      <Banner />
+      <Gallery />
       <Footer />
     </div>
   );
