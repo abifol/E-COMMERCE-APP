0e39147c7e4677fb0027883df752ab6c95979caa
0xdennis <meneweifeanyi5@gmail.com>
2024-03-05 10:30:17 +0100

    initial commit

---
+++ src/pages/prints.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from '../components/navbar';
+import Hero from '../components/hero';
+import Footer from '../components/footer';
+const prints = () => {
+  return (
+    <div>
+        <Navbar/>
+        <Hero/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default prints
